09e1d2b3e7ae77f01664ad0a930844d2aaa8bc67
Rimuru <93518654+RimuruVN@users.noreply.github.com>
2021-11-03 11:15:22 +0700

    Update forcesub.py

--- helpers/forcesub.py
+++ helpers/forcesub.py
@@ -13,8 +13,8 @@ async def ForceSub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.chat.id,
-                text="Sorry Unkil, You are Banned! You will be Kicked from This Group within 15 Seconds.\n"
-                     "Contact: [Support Group](https://t.me/linux_repo).",
+                text="Xin lỗi, bạn bị cấm! Bạn sẽ được Kicked khỏi Nhóm này trong vòng 15 giây.\n"
+                     "Liên hệ: [@Gimsuri](https://t.me/gimsuri).",
                 disable_web_page_preview=True,
                 reply_to_message_id=cmd.message_id
             )
@@ -24,22 +24,22 @@ async def ForceSub(bot: Client, cmd: Message):
         try:
             invite_link = await bot.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL))
         except FloodWait as e:
-            print(f"Sleep of {e.x}s caused by FloodWait")
+            print(f"Ngủ trong {e.x}s bởi lỗi FloodWait")
             await asyncio.sleep(e.x)
             return 200
         except Exception as err:
-            print(f"Unable to do Force Subscribe to {Config.UPDATES_CHANNEL}\n\nError: {err}")
+            print(f"Không thể thực hiện Buộc Đăng Ký{Config.UPDATES_CHANNEL}\n\nError: {err}")
             return 200
         send_ = await bot.send_message(
             chat_id=cmd.chat.id,
             text=f"Hello {cmd.from_user.mention},\n\n"
-                 f"**Please [Join Channel]({invite_link.invite_link}) to Send Messages to This Group!**\n\n"
-                 "Till you join channel you will be muted in this group!",
+                 f"**Hãy [tham gia kênh]({invite_link.invite_link}) để gửi tin nhắn cho nhóm này!**\n\n"
+                 "Cho đến khi tham gia kênh, bạn sẽ bị tắt tiếng trong nhóm này!",
             reply_to_message_id=cmd.message_id,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("🤖 Join Channel 🤖", url=invite_link.invite_link)]
+                    [InlineKeyboardButton("🤖 Tham gia kênh 🤖", url=invite_link.invite_link)]
                 ]
             )
         )
