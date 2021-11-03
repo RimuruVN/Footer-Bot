6e827d153413563c4e04211ed809ed5efb1a8ece
Rimuru <93518654+RimuruVN@users.noreply.github.com>
2021-11-03 11:21:40 +0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,10 +11,10 @@ class Config(object):
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 2072962525))
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
     START_TEXT = """
-Hi, I am Channel Broadcast Footer Bot!
+Xin chào, tôi là Bot Footer Broadcast!
 
-I can add footer to Channel Media Messages. Just add me to the channel as Admin with all rights and setup /settings !!
+Tôi có thể thêm chân trang vào Tin nhắn Phương tiện Kênh. Chỉ cần thêm tôi vào kênh với tư cách là Quản trị viên với tất cả các quyền và thiết lập /settings !!
 """
